c25cb50ce0de628e28613941bafebec61a6f1c2d
smcilvenny <s.mcilvenny@students.clark.edu>
2019-10-21 01:55:18 -0700

    DONE FROM HOME

---
+++ first homework assignment/first homework assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_homework_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Sean McIlvenny and I am coding C#!");
+            Console.WriteLine("Sorry this is late but I am just trying to catch up on missing work!");
+            Console.WriteLine("C# is alot harder than python or javascript!!!");
+        }
+    }
+}
